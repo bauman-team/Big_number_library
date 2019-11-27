d8eabf20f56c7bbc044e1b3f21e8e2829f7b9201
Mikhail Baryshnikov <misha0401024@rambler.ru>
2019-11-27 18:46:37 +0300

    убрал лишние тесты

--- long_arifmetics/main.cpp
+++ long_arifmetics/main.cpp
@@ -15,68 +15,6 @@ int main()
 		//cout << x - y;
 		cout << endl;
 	}
-
-	/*cout << endl;
-	cout << "x + y = " << x + y << endl;
-
-	cout << endl;
-	cout << "x + 15 = " << x + 15 << endl;
-	x += 15;
-	cout << "x += 15 = " << x << endl;
-
-	cout << "x + 1'000'000'001 = " << x + 1'000'000'001 << endl;
-	x += 1'000'000'001;
-	cout << "x += 1'000'000'001 = " << x << endl;
-
-	cout << endl;
-	y = x;
-	x = 2'000'000'002;
-	cout << "x = " << x << endl;
-	cout << "y = " << y << endl;
-	cout << "x + y = " << x + y << endl;
-	x += y;
-	cout << "x += y = " << x << endl;
-	
-	cout << endl;
-	cout << "x = " << x << endl;
-	cout << "y = " << y << endl;
-	if (x == y)
-		cout << "x == y" << endl;
-	else
-		cout << "x != y" << endl;
-	x = y;
-	cout << "x = " << x << endl;
-	cout << "y = " << y << endl;
-	if (x == y)
-		cout << "x == y" << endl;
-	else
-		cout << "x != y" << endl;
-
-
-	cout << endl;
-	cout << "x = " << x << endl;
-	cout << "x++ = " << x++ << endl;
-	cout << "x++ = " << x++ << endl;
-	cout << "++x = " << ++x << endl;
-	cout << "x = " << x << endl;
-
-	y = x++;
-	
-	if (x < y)
-		cout << "x < y" << endl;
-
-	if (y < x)
-		cout << "y < x" << endl;
-
-	cin >> x;
-	cin >> y;
-
-	cout << "x = " << x << endl;
-	cout << "y = " << y << endl;
-	cout << "x - y = " << x - y << endl;
-	cout << "y - x = " << y - x << endl;*/
-
-
 	system("pause");
 	return 0;
 }
