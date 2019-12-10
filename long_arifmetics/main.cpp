56171b99e4fcf495bc5333d6289b1e0865f3722a
Артём Бабин <artem031201@yandex.ru>
2019-12-10 21:12:03 +0300

    Revert "Fixed 400+ errors"
    This reverts commit 7b6a9656752eac4385a2450e11070ffe6281d057.

--- long_arifmetics/main.cpp
+++ long_arifmetics/main.cpp
@@ -3,17 +3,33 @@
 int main()
 {
 	Big_number x, y, result;
+	char sign;
 
 	while (true)
 	{
 		cout << "x: ";
 		cin >> x;
+		cout << "sign: ";
+		cin >> sign;
 		cout << "y: ";
 		cin >> y;
-		cout << endl;
-		cout << "x * y: " << (x * y);
-		//cout << x - y;
-		cout << endl;
+		cout << "Answer: ";
+		switch (sign)
+		{
+		case '+':
+			cout << x + y;
+			break;
+		case '-':
+			cout << x - y;
+			break;
+		case '*':
+			cout << x * y;
+			break;
+		case '/':
+			cout << x / y;
+			break;
+		}
+		cout << endl << endl;
 	}
 	system("pause");
 	return 0;
--- long_arifmetics/struct_big_number.h
+++ long_arifmetics/struct_big_number.h
@@ -14,8 +14,8 @@ struct Big_number
 	bool isNegative = false;
 	static void string_to_big_number(Big_number& number, string s_x);
 	static void int_to_big_number(Big_number& number, int i_x);
-	friend ostream& operator<< (ostream &out, Big_number number);
-	friend istream& operator>> (istream &in, Big_number& number);
+	friend ostream& operator<< (ostream& out, Big_number number);
+	friend istream& operator>> (istream& in, Big_number& number);
 	Big_number operator+ (Big_number b); // Big_number + Big_number
 	Big_number operator- (Big_number b); // Big_number - Big_number
 	Big_number operator+ (int n); // Big_number + int
@@ -38,5 +38,9 @@ struct Big_number
 	bool operator!= (int);
 	bool operator<= (Big_number);
 	bool operator>= (Big_number);
+	Big_number operator/ (Big_number b);
+	Big_number operator--();
+	Big_number operator--(int);
+	Big_number operator-(int);
+	
 };
-
