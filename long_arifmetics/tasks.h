5af14e663b0dd96b27efa8e5f8033ec0f03f535b
Артём Бабин <artem031201@yandex.ru>
2019-12-26 14:14:23 +0300

    Убрал случайно добавленный кусок кода в tasks.h

--- long_arifmetics/tasks.h
+++ long_arifmetics/tasks.h
@@ -1,16 +1,3 @@
 #pragma once
 // добавить класс Big_Double наследовать Big_Int
 // добавить геттеры и сеттеры ???
-
-int LEN = 80 / n - 1;
-cout << scientific << setprecision(LEN - 6);
-for (int i = 0; i < n; i++)
-{
-	for (int j = 0; j < n; j++)
-	{
-		cout << matrix[i][j];
-		if (j != n - 1)
-			cout << " ";
-	}
-	cout << endl << endl;
-}
