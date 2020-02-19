12ef784f7699cdb98c32f2113a71b36c06870014
mikhail-xnor <misha1524154@yandex.ru>
2020-02-19 22:57:03 +0300

    исправил ошибки ввода и вывода, добавил коментарии

--- long_arifmetics/operators_Big_Double.cpp
+++ long_arifmetics/operators_Big_Double.cpp
@@ -3,13 +3,18 @@
 std::istream &operator>>(std::istream &stream, Big_Double& number)
 {
 	int position;
+	register int i = 0;
 	std::string s_x;
 	std::cin >> s_x;
 	if (!Big_Int::is_valid_format(s_x))
-		throw std::exception("Invalid number!");
-	if (s_x.find('.') != std::string::npos)
-		s_x.erase((position = s_x.find('.')), 1);
-	position = s_x.length() - position;
+		throw std::exception("Invalid number!"); // проверка правильности ввода такая же для Big_Int
+	if ((position = s_x.find('.')) != std::string::npos) // определение позиции точки с начала числа
+	{
+		while (s_x[s_x.length() - i - 1] == '0') // удаление незначащих нулей в дробной части
+			s_x.erase(s_x.length() - i - 1, 1);
+		s_x.erase(position, 1);
+	}
+	position = position == std::string::npos ? 0 : s_x.length() - position; // отсчет позиции точки с конца числа
 	number.num.clear();
 	number.isNegative = false;
 	Big_Int::string_to_big_number(number, s_x);
@@ -25,9 +30,9 @@ std::ostream &operator<<(std::ostream & stream, Big_Double &number)
 	for (i = number.num.size() - 1; i >= 0; i--)
 	{
 		int buffer = 100'000'000;
-		if (!(number.index_of_point >= i * 9 && number.index_of_point < (i + 1) * 9))
+		if (!(number.index_of_point >= i * 9 && number.index_of_point < (i + 1) * 9)) // если точка не попадает в полуинтервал элемента вектора включая последнюю позицию(0)
 		{
-			if (i != number.num.size() - 1)
+			if (i != number.num.size() - 1) // стандартный вывод числа (как у Big_Int)
 				while (buffer > number.num[i] && buffer)
 				{
 					stream << 0;
@@ -39,15 +44,15 @@ std::ostream &operator<<(std::ostream & stream, Big_Double &number)
 		else
 		{
 			int num = number.num[i];
-			for (int j = 8; j != -1; --j)
+			for (int j = 8; j != -1; --j) // перебор всех позиций числа
 			{
-				if (i != number.num.size() - 1 || num / buffer)
+				if (i != number.num.size() - 1 || number.num[i] / buffer) // если число выводится не первым или первый не 0
 				{
 					stream << num / buffer;
 					num %= buffer;
 					buffer /= 10;
 				}
-				else if (number.index_of_point == i * 9 + j)
+				else if (number.index_of_point == i * 9 + j) //если следующий элемент точка вывести 0
 				{
 					stream << 0;
 					num %= buffer;
@@ -58,7 +63,7 @@ std::ostream &operator<<(std::ostream & stream, Big_Double &number)
 					num %= buffer;
 					buffer /= 10;
 				}
-				if (number.index_of_point == i * 9 + j)
+				if (number.index_of_point == i * 9 + j && number.index_of_point != 0) // если позиция совпадает с поз. точки и ее положение не в конце  
 				{
 					stream << '.';
 				}
